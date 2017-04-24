5e44108df70d2bf860af04e15aa4cfe6f8b0bfea
Johannes Seidel <jojo.seidel345@web.de>
2017-04-24 15:41:26 +0200

    Trying to avoid superaids aka merge problems.

--- src/main/java/edu/hm/cs/swa/model/Disc.java
+++ src/main/java/edu/hm/cs/swa/model/Disc.java
@@ -2,4 +2,68 @@ package edu.hm.cs.swa.model;
 
 public class Disc extends Medium {
 
+    String barcode, director;
+    int fsk;
+    
+    Disc(String barcode, String director, int fsk, String title) {
+        super(title);
+        this.barcode = barcode;
+        this.director = director;
+        this.fsk = fsk;
+    }
+    
+    Disc(){
+        
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((barcode == null) ? 0 : barcode.hashCode());
+        result = prime * result + ((director == null) ? 0 : director.hashCode());
+        result = prime * result + fsk;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Disc other = (Disc) obj;
+        if (barcode == null) {
+            if (other.barcode != null)
+                return false;
+        } else if (!barcode.equals(other.barcode))
+            return false;
+        if (director == null) {
+            if (other.director != null)
+                return false;
+        } else if (!director.equals(other.director))
+            return false;
+        if (fsk != other.fsk)
+            return false;
+        return true;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getFsk() {
+        return fsk;
+    }
+    
+    @Override
+    public String toString() {
+        return "";
+    }
 }
--- src/main/java/edu/hm/cs/swa/model/Medium.java
+++ src/main/java/edu/hm/cs/swa/model/Medium.java
@@ -1,5 +1,42 @@
 package edu.hm.cs.swa.model;
 
 public class Medium {
+    String title;
 
+    Medium(String title) {
+        this.title = title;
+    }
+
+    Medium() {
+        
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Medium other = (Medium) obj;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
+            return false;
+        return true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
