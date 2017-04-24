fa8290c83b6b6da57d23cd6b5aed38d8273a00df
Michael Reile <stimmungswecker@googlemail.com>
2017-04-24 15:35:00 +0200

    Disc

--- src/main/java/edu/hm/cs/swa/model/Disc.java
+++ src/main/java/edu/hm/cs/swa/model/Disc.java
@@ -2,4 +2,18 @@ package edu.hm.cs.swa.model;
 
 public class Disc extends Medium {
 
+    private final String barcode;
+
+    private final String director;
+
+    private final int fsk;
+
+    public Disc() {
+
+    }
+
+    public Disc() {
+
+    }
+
 }
