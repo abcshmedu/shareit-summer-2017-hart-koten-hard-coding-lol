9dea7a7d0813774f67bfbe746ddcbeb8ecff7ba8
Michael Reile <stimmungswecker@googlemail.com>
2017-04-24 15:44:25 +0200

    Book and Copy

--- src/main/java/edu/hm/cs/swa/model/Book.java
+++ src/main/java/edu/hm/cs/swa/model/Book.java
@@ -1,5 +1,58 @@
 package edu.hm.cs.swa.model;
 
-public class Book extends Medium{
+public class Book extends Medium {
 
+    private final String author;
+
+    private final String isbn;
+
+
+    public Book() {
+
+    }
+
+
+    public Book(final String author, final String isbn, final String) {
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Book book = (Book) o;
+
+        if (!author.equals(book.author)) return false;
+        return isbn.equals(book.isbn);
+    }
+
+
+    @Override
+    public int hashCode() {
+        int result = author.hashCode();
+        result = 31 * result + isbn.hashCode();
+        return result;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+               "author='" + author + '\'' +
+               ", isbn='" + isbn + '\'' +
+               '}';
+    }
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+
+    public String getIsbn() {
+        return isbn;
+    }
 }
--- src/main/java/edu/hm/cs/swa/model/Copy.java
+++ src/main/java/edu/hm/cs/swa/model/Copy.java
@@ -2,4 +2,24 @@ package edu.hm.cs.swa.model;
 
 public class Copy {
 
+    private final Medium medium;
+
+    private String owner;
+
+
+
+    public Copy(final Medium medium, final String owner) {
+        this.medium = medium;
+        this.owner = owner;
+    }
+
+
+    public Medium getMedium() {
+        return medium;
+    }
+
+
+    public String getOwner() {
+        return owner;
+    }
 }
