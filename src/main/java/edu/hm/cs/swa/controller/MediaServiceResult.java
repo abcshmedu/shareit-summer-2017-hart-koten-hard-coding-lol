6ec0d52722da4a3e4af4a3108f337c54f0daa2ee
Johannes Seidel <jojo.seidel345@web.de>
2017-05-04 14:38:07 +0200

    hik

--- src/main/java/edu/hm/cs/swa/controller/MediaServiceResult.java
+++ src/main/java/edu/hm/cs/swa/controller/MediaServiceResult.java
@@ -9,7 +9,7 @@ import javax.ws.rs.core.Response.Status;
 public enum MediaServiceResult {
 
     OK(Status.OK.getStatusCode(), Status.OK), INVALID_ISBN(Status.NOT_ACCEPTABLE.getStatusCode(),
-            Status.NOT_ACCEPTABLE), ISBN_TAKEN(301, Status.GONE), AUTHOR_OR_TITLE_MISSING(
+            Status.NOT_ACCEPTABLE), ISBN_TAKEN(Status.GONE.getStatusCode(), Status.GONE), AUTHOR_OR_TITLE_MISSING(
                     Status.PARTIAL_CONTENT.getStatusCode(), Status.PARTIAL_CONTENT), ISBN_NOT_FOUND(
                             Status.NOT_FOUND.getStatusCode(), Status.NOT_FOUND), MODIFIY_ISBN(
                                     Status.NOT_MODIFIED.getStatusCode(), Status.NOT_MODIFIED), AUTHOR_AND_TITLE_MISSING(
