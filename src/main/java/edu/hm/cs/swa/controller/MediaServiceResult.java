a7c3ba4fa714f40da811906d67c49a8bf40f12de
Johannes Seidel <jojo.seidel345@web.de>
2017-05-04 14:03:14 +0200

    MediaServiceResult enum advanced form

--- src/main/java/edu/hm/cs/swa/controller/MediaServiceResult.java
+++ src/main/java/edu/hm/cs/swa/controller/MediaServiceResult.java
@@ -1,8 +1,27 @@
 package edu.hm.cs.swa.controller;
 
+import javax.net.ssl.SSLEngineResult.Status;
+
 /**
- * Created by CaptainEinsicht on 03.05.2017.
- * Alle Rechte vorbehalten bei Missbrauch wird zurück Missbraucht
+ * Created by CaptainEinsicht on 03.05.2017. Alle Rechte vorbehalten bei
+ * Missbrauch wird zurück Missbraucht
  */
 public enum MediaServiceResult {
+
+    OK(200), INVALID_ISBN(300), ISBN_TAKEN(301), AUTHOR_OR_TITLE_MISSING(302), ISBN_NOT_FOUND(303),
+    MODIFIY_ISBN(304), AUTHOR_AND_TITLE_MISSING(305);
+
+    private int statusCode;
+
+    private MediaServiceResult(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
 }
