edcfc86ea9953aec0582a22bc4925fd9e4be7dd9
Johannes Seidel <jojo.seidel345@web.de>
2017-05-07 22:36:19 +0200

    Testklassen Skeletons

---
+++ src/main/java/edu/hm/cs/swa/Tests/MediaResourceTest.java
@@ -0,0 +1,14 @@
+package edu.hm.cs.swa.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MediaResourceTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ src/main/java/edu/hm/cs/swa/Tests/MediaServiceTest.java
@@ -0,0 +1,14 @@
+package edu.hm.cs.swa.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MediaServiceTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
