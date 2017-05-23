0e2aa0807b5713a4a8b44f5e99271d608dc4f18a
Michael Reile <stimmungswecker@googlemail.com>
2017-05-23 11:49:51 +0200

    user

---
+++ src/main/java/edu/hm/cs/swa/model/User.java
@@ -0,0 +1,44 @@
+package edu.hm.cs.swa.model;
+
+/**
+ * Created by CaptainEinsicht on 23.05.2017.
+ * Alle Rechte vorbehalten bei Missbrauch wird zurück Missbraucht
+ */
+public class User {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String password;
+
+    private int age;
+
+
+    public User(final String firstName, final String lastName, final String password, final int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.age = age;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+}
