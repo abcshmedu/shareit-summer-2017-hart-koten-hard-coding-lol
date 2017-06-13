89b0ab26f3c5ed6b09dbac8911e3b1d92a980e77
Johannes Seidel <jojo.seidel345@web.de>
2017-06-13 14:59:26 +0200

    di ioc

--- src/main/java/edu/hm/cs/swa/controller/MediaPersistenceImpl.java
+++ src/main/java/edu/hm/cs/swa/controller/MediaPersistenceImpl.java
@@ -3,5 +3,5 @@ package edu.hm.cs.swa.controller;
 /**
  * Created by Jojo on 13.06.2017.
  */
-public class MediaPersistenceImpl {
+public class MediaPersistenceImpl implements MediaPersistence{
 }
--- src/main/java/edu/hm/cs/swa/guice/ShareitServletContextListener.java
+++ src/main/java/edu/hm/cs/swa/guice/ShareitServletContextListener.java
@@ -3,5 +3,43 @@ package edu.hm.cs.swa.guice;
 /**
  * Created by Jojo on 13.06.2017.
  */
-public class ShareitServletContextListener {
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
+
+import edu.hm.cs.swa.controller.MediaPersistence;
+import edu.hm.cs.swa.controller.MediaPersistenceImpl;
+import edu.hm.cs.swa.controller.MediaService;
+import edu.hm.cs.swa.controller.MediaServiceImpl;
+
+
+/**
+ * Context Listener to enable usage of google guice together with jersey.
+ * @author <a mailto:axel.boettcher@hm.edu>Axel B&ouml;ttcher</a>
+ *
+ */
+public class ShareitServletContextListener extends GuiceServletContextListener {
+
+    private static final Injector injector = Guice.createInjector(new ServletModule() {
+        @Override
+        protected void configureServlets() {
+            bind(MediaService.class).to(MediaServiceImpl.class);
+            bind(MediaPersistence.class).to(MediaPersistenceImpl.class);
+        }
+    });
+
+    @Override
+    protected Injector getInjector() {
+        return injector;
+    }
+
+    /**
+     * This method is only required for the HK2-Guice-Bridge in the Application class.
+     * @return Injector instance.
+     */
+    static Injector getInjectorInstance() {
+        return injector;
+    }
+
 }
